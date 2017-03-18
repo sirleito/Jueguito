3ab6debe1db5813e4abe41fba10297f7a0c17247
leovenezia <leovenezia@outlook.com.ar>
2017-03-18 15:46:02 -0300

    arreglado el error al morir cada enemigo

--- js/index.js
+++ js/index.js
@@ -6,35 +6,35 @@ document.addEventListener('DOMContentLoaded',function(){
 			this.vida = v;
 			this.imagen = new Image();
 			this.imagen.src = i;
-			this.imagen.className = "imagen"
-			this.especial = e
+			this.imagen.className = "imagen";
+			this.especial = e;
 			this.rabia = r
 		}
 		mostrar(){
-	  	var identificador = this.nombre
-	  	div.innerHTML = div.innerHTML + '<div id=' + identificador + ' class="personaje"></div>'
+	  	var identificador = this.nombre;
+	  	div.innerHTML = div.innerHTML + '<div id=' + identificador + ' class="personaje"></div>';
 	  	var div2 = document.getElementById(identificador);
 	  	div2.appendChild(this.imagen);
-	  	div2.innerHTML = div2.innerHTML + '<h3>' + this.nombre + '</h3></br>' + '<p>Vida: ' + this.vida + '</p>' + '<p>Fuerza: ' + this.fuerza + '</p>' + '<p>Especial: ' + this.especial + '</p>'+ '<p>Rabia: ' + this.rabia + '</p>'
+	  	div2.innerHTML = div2.innerHTML + '<h3>' + this.nombre + '</h3></br>' + '<p>Vida: ' + this.vida + '</p>' + '<p>Fuerza: ' + this.fuerza + '</p>' + '<p>Especial: ' + this.especial + '</p>'+ '<p>Rabia: ' + this.rabia + '</p>';
 	   	gladiador = document.getElementById("Gladiador");
 		guerrero = document.getElementById("Guerrero");
 		asesina = document.getElementById("Asesina");
 	  }
 	  combate(){
-	  	div.innerHTML = div.innerHTML + "<div id=" + this.nombre + "></div>"
-	  	var div2 = document.getElementById(this.nombre)
-	  	var rabia = this.rabia
+	  	div.innerHTML = div.innerHTML + "<div id=" + this.nombre + "></div>";
+	  	var div2 = document.getElementById(this.nombre);
+	  	var rabia = this.rabia;
 	  	var segundoId = this.nombre + "2";
 	  	var tercerId = this.nombre + "3";
 	  	var cuartoId = this.nombre + "4";
 	  	var quintoId = this.nombre + "5";
 	  	var vidaValor = this.vida;
 	  	div2.appendChild(this.imagen);
-	  	div2.innerHTML = div2.innerHTML + "<p id=" + segundoId +">Vida:" + vidaValor + "</p><p>Especial</p><div class='especial' id="+ tercerId + "></div><button value=" + this.nombre + " class='boton'>¡Atacar!</button>"
+	  	div2.innerHTML = div2.innerHTML + "<p id=" + segundoId +">Vida:" + vidaValor + "</p><p>Especial</p><div class='especial' id="+ tercerId + "></div><button value=" + this.nombre + " class='boton'>¡Atacar!</button>";
 	  	datos.push(this.vida);
 	  	datos.push(this.fuerza);
 	  	datos.push(this.rabia);
-	  	datos.push(this.especial)
+	  	datos.push(this.especial);
 	  	for (var i = rabia; i > 0; i--) {
 	  		var rabiaId = document.getElementById(tercerId);
 	  		rabiaId.innerHTML = rabiaId.innerHTML + "<div class='rabia " + quintoId + "'></div>";
@@ -45,17 +45,21 @@ document.addEventListener('DOMContentLoaded',function(){
 	//Los personajes*//
 
 	var div = document.getElementById('div');
-	var clasePersonajes = document.getElementsByClassName("personaje")
+	var clasePersonajes = document.getElementsByClassName("personaje");
 	
-	var personajes = [];
-	personajes.push(new heroes("Gladiador", 50, 1000, "img/hombre.png", "Puñote", 7));
-	personajes.push(new heroes("Guerrero", 100, 500, "img/gato.png", "Ejecucion", 6));
-	personajes.push(new heroes("Asesina", 75, 750, "img/mujer.png", "Degradación", 4));
+	var personajes = [
+		new heroes("Gladiador", 50, 1000, "img/hombre.png", "Puñote", 7),
+		new heroes("Guerrero", 100, 500, "img/gato.png", "Ejecucion", 6),
+		new heroes("Asesina", 75, 750, "img/mujer.png", "Degradación", 4),
+	];
+	
 
-	var malos = [];
-	malos.push(new heroes("Zombie", 10, 500, "img/zombie.png", "Poder", 5));
-	malos.push(new heroes("Alien", 20, 600, "img/alien.png", "Poder", 5));
-	malos.push(new heroes("Bruja", 30, 1000, "img/bruja.png", "Poder", 4));
+	var malos = [
+		new heroes("Zombie", 10, 500, "img/zombie.png", "Poder", 5),
+		new heroes("Alien", 20, 600, "img/alien.png", "Poder", 5),
+		new heroes("Bruja", 30, 1000, "img/bruja.png", "Poder", 4),
+	];
+	
 
 	var datos = [];
 
@@ -63,9 +67,9 @@ document.addEventListener('DOMContentLoaded',function(){
 //Comienzo
 
 	function funcStart() {
-		start.style.display = "none"
+		start.style.display = "none";
 		for(var p of personajes){
-			p.mostrar()	
+			p.mostrar();
 		}
 		eleccion();
 	}
@@ -73,7 +77,7 @@ document.addEventListener('DOMContentLoaded',function(){
 	var start = document.getElementById("start");
 
 	start.onclick = function(){
-		funcStart()
+		funcStart();
 	}
 
 //Eleccion de los personajes
@@ -84,37 +88,37 @@ document.addEventListener('DOMContentLoaded',function(){
 	var heroeElegido;
 	var contadorMalos = 0;
 	var divMalo;
-	var divBueno
-	var maloElegido
-	var maloRabia
+	var divBueno;
+	var maloElegido;
+	var maloRabia;
 
 	function seleccionMalo(){
 
-		malos[contadorMalos].combate()			
-		maloElegido = malos[contadorMalos]
-		maloRabia = maloElegido.nombre + "5"
-		divMalo = document.getElementById(maloElegido.nombre)
+		malos[contadorMalos].combate();	
+		maloElegido = malos[contadorMalos];
+		maloRabia = maloElegido.nombre + "5";
+		divMalo = document.getElementById(maloElegido.nombre);
 	}
 	function seleccion(e){
 
-			div.innerHTML = ""
-			e.combate()
+			div.innerHTML = "";
+			e.combate();
 			heroeElegido = e;
-			heroeRabia = e.nombre + "5"
-			divBueno = document.getElementById(heroeElegido.nombre)
-			seleccionMalo()
+			heroeRabia = e.nombre + "5";
+			divBueno = document.getElementById(heroeElegido.nombre);
+			seleccionMalo();
 			ataque();
 		};
 	function eleccion(){
 		
 		gladiador.onclick = function(){
-			seleccion(personajes[0])
+			seleccion(personajes[0]);
 		};
 		guerrero.onclick = function(){
-			seleccion(personajes[1])
+			seleccion(personajes[1]);
 		};
 		asesina.onclick = function(){
-			seleccion(personajes[2])
+			seleccion(personajes[2]);
 		};
 	}
 
@@ -124,7 +128,7 @@ document.addEventListener('DOMContentLoaded',function(){
 	var heroeRabia;
 	var contadorRabia = 0;
 	var contadorRabiaMala = 0;
-	var rabiaMala
+	var rabiaMala;
 
 	function ataque(){
 		var perBueno = document.getElementsByClassName("boton")[0];
@@ -135,26 +139,27 @@ document.addEventListener('DOMContentLoaded',function(){
 		var nombreBueno = perBueno.value;
 		var vidaBuenoId = nombreBueno + "2";
 		var vidaBueno = document.getElementById(vidaBuenoId);
-		perMalo.className += " none"
+		perMalo.className += " none";
 
 		perBueno.onclick = function(){
-			var cantRabia = document.getElementsByClassName(heroeRabia).length
-			var rabiaHeroe = document.getElementsByClassName(heroeRabia)[contadorRabia]
-			var cantRabiaMala = document.getElementsByClassName(maloRabia).length
-			var rabiaMala = document.getElementsByClassName(maloRabia)[contadorRabiaMala]
+			var cantRabia = document.getElementsByClassName(heroeRabia).length;
+			var rabiaHeroe = document.getElementsByClassName(heroeRabia)[contadorRabia];
+			var cantRabiaMala = document.getElementsByClassName(maloRabia).length;
+			var rabiaMala = document.getElementsByClassName(maloRabia)[contadorRabiaMala];
 			
 			
 			rabiaBueno();
 			rabiaMalo();
 
-			setTimeout(function(){fondoRojo(divMalo)}, 200)
-			setTimeout(function(){fondoRojo(document.getElementById(heroeElegido.nombre))}, 400)
+			setTimeout(function(){fondoRojo(divMalo)}, 200);
+			setTimeout(function(){fondoRojo(document.getElementById(heroeElegido.nombre))}, 400);
 
 			function fondoRojo(a){
-					a.className = "fondoRojo"
+				if(!a)return;
+					a.className = "fondoRojo";
 					setTimeout(function(){
 					a.className = ""
-				}, 200)
+				}, 200);
 
 			};
 				
@@ -162,51 +167,45 @@ document.addEventListener('DOMContentLoaded',function(){
 			
 			function rabiaBueno(){
 				if ( cantRabia > contadorRabia){
-					rabiaHeroe.style.background = "green"
-					rabiaHeroe.style.border = "solid 1px green"
-					contadorRabia ++
+					rabiaHeroe.style.background = "green";
+					rabiaHeroe.style.border = "solid 1px green";
+					contadorRabia ++;
 					datos[4] = datos[4] - datos[1];
 				}else{
-					dañoRabia = datos[1] * 2
+					dañoRabia = datos[1] * 2;
 
-					alert("¡Ataque especial de tu personaje! " + datos[3] + " daño: " + dañoRabia)
-					contadorRabia = 0
-					datos[4] = datos[4] - dañoRabia
+					alert("¡Ataque especial de tu personaje! " + datos[3] + " daño: " + dañoRabia);
+					contadorRabia = 0;
+					datos[4] = datos[4] - dañoRabia;
 
 					for (var i = 0; i < cantRabia; i++) {
-						var rabiaRoja = document.getElementsByClassName(heroeRabia)[i]
-						rabiaRoja.style.background = "#f44336"
-						rabiaRoja.style.border = "solid 1px #ef5350"
+						var rabiaRoja = document.getElementsByClassName(heroeRabia)[i];
+						rabiaRoja.style.background = "#f44336";
+						rabiaRoja.style.border = "solid 1px #ef5350";
 					}
 				};
 				vidaMalo.innerHTML = "Vida: " + datos[4];
-				
-				//divMalo.style.background = "#f44336"
-				//setTimeout(function(){
-				//	divMalo.style.background = ""
-				//}, 300)
-				//console.log(divMalo)
 				
 			}
 
 			function rabiaMalo(){
 				if ( cantRabiaMala > contadorRabiaMala){
-					rabiaMala.style.background = "green"
-					rabiaMala.style.border = "solid 1px green"
-					contadorRabiaMala ++
+					rabiaMala.style.background = "green";
+					rabiaMala.style.border = "solid 1px green";
+					contadorRabiaMala ++;
 					datos[0] = datos[0] - datos[5];
 				}else{
-					dañoRabiaMala = datos[5] * 2
+					dañoRabiaMala = datos[5] * 2;
 
-					alert("¡Ataque especial de tu enemigo! " + datos[7] + " daño: " + dañoRabiaMala)
-					contadorRabiaMala = 0
-					datos[0] = datos[0] - dañoRabiaMala
+					alert("¡Ataque especial de tu enemigo! " + datos[7] + " daño: " + dañoRabiaMala);
+					contadorRabiaMala = 0;
+					datos[0] = datos[0] - dañoRabiaMala;
 
 					for (var i = 0; i < cantRabiaMala; i++) {
 
-						var rabiaRojaMala = document.getElementsByClassName(maloRabia)[i]
-						rabiaRojaMala.style.background = "#f44336"
-						rabiaRojaMala.style.border = "solid 1px #ef5350"
+						var rabiaRojaMala = document.getElementsByClassName(maloRabia)[i];
+						rabiaRojaMala.style.background = "#f44336";
+						rabiaRojaMala.style.border = "solid 1px #ef5350";
 					}
 					
 				};
@@ -229,25 +228,25 @@ document.addEventListener('DOMContentLoaded',function(){
 
 
 			if (datos[0] <= "0") {
-				div.innerHTML = "<h2 class='resultado'>¡Perdiste, idiota!</h2>"
+				div.innerHTML = "<h2 class='resultado'>¡Perdiste, idiota!</h2>";
 			}
 			if (datos[4] <= "0" && contadorMalos <= 2) {
-				div.innerHTML = "<h2 class='resultado'>¡Enemigo derrotado!</h2><button id='continuar' class='continuar'>Continuar con la masacre</button>"
-		 		contadorMalos ++
+				div.innerHTML = "<h2 class='resultado'>¡Enemigo derrotado!</h2><button id='continuar' class='continuar'>Continuar con la masacre</button>";
+		 		contadorMalos ++;
 
 		 		
 				var continuar = document.getElementById("continuar");
 				continuar.onclick = function(){
-				contadorRabia = 0
-		 		contadorRabiaMala = 0
-				div.innerHTML = ""
-				datos = []
+				contadorRabia = 0;
+		 		contadorRabiaMala = 0;
+				div.innerHTML = "";
+				datos = [];
 				heroeElegido.combate();
-				seleccionMalo()
-				ataque();	
+				seleccionMalo();
+				ataque();
 				} 
 			if (datos[4] <= "0" && contadorMalos == 3){
-				div.innerHTML = "<h1 class='puto'>Sos el puto amo</h1>"
+				div.innerHTML = "<h1 class='puto'>Sos el puto amo</h1>";
 				}
 
 			};
